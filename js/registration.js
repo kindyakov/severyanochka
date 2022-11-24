8d4dd206cab1bfa891716ed8a7dd8ddbff948c15
kindyakov <kindyakov_2016@mail.ru>
2022-11-25 01:14:20 +0300

    css/about.css 	css/adaptiv.css 	css/artickes.css 	css/basket.css 	css/cards_products.css 	css/catalog.css 	css/contacts.css 	css/fonts.css 	css/openings.css 	css/product-groups.css 	css/registration.css

--- js/registration.js
+++ js/registration.js
@@ -14,60 +14,87 @@ window.addEventListener('DOMContentLoaded', () => {
   const inputPass = document.querySelector('input[name="password"]');
   const inputConfirmPass = document.querySelector('input[name="confirm-password"]');
 
+  const inputEmail = document.querySelector('input[name="email"]');
+  const inputCard = document.querySelector('input[name="card"]');
+
   const pattern = {
     phone: /^(\s*)?(\+)?([- _():=+]?\d[- _():=+]?){11}(\s*)?$/,
     name: /[а-яА-ЯЁё]/,
-    surname: /[а-яА-ЯЁё]/,
     region: /[а-яА-ЯЁё]/,
     city: /[а-яА-ЯЁё]/,
     email: /^[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}$/i,
     password: /^[a-zA-Z0-9а-яА-ЯЁё]{5,}$/,
     data: /^[0-9]+$/,
+    card: /^[0-9]+$/,
   }
-  // email
-  // /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i; 
 
-  // /^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[1,3-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/
+  let error = 0;
+
   input.forEach(inputs => {
     inputs.addEventListener('input', () => {
-      const item = inputs.closest('.registration__item');
-      item.classList.remove('error');
-      if (inputs.type === 'password') validPassword();
+      removeError(inputs);
+      if (inputs.type === 'password') {
+        validPassword();
+      };
     });
   });
 
   form.addEventListener('submit', function (e) {
-    const items = document.querySelectorAll('.registration__item');
+    e.preventDefault();
+    error = 0;
     inputRequired.forEach(inputs => {
-      const item = inputs.closest('.registration__item');
-      const infoError = item.querySelector('.info-error');
       const inputName = inputs.name;
       const inputData = inputs.dataset.valid;
 
-      if (inputs.value === '') {
-        item.classList.add('error');
-        infoError.textContent = 'Пустое поле !';
-        e.preventDefault();
-        console.log('pusto');
-        return;
-      } else {
-        if (!pattern[inputData].test(inputs.value.trim())) {
-          item.classList.add('error');
-          infoError.textContent = 'Не правильно заполнено поле !';
-          e.preventDefault();
-          console.log('valide');
-        }
-      }
+      if (inputs.value === '') addError(inputs, 'Пустое поле !');
+      else if (inputName === 'phone') validPhone(inputs);
+      else if (inputData === 'name') validName(inputs);
+      else if (inputName === 'confirm-password') validPassword();
     });
-    items.forEach(tem => { if (tem.classList.contains('error')) e.preventDefault() });
-  });
+    if (inputEmail.value !== '') validEmail(inputEmail);
+    if (inputCard.value !== '') validСard(inputCard);
 
-  function validPassword() {
-    if (inputPass.value !== inputConfirmPass.value) {
-      const item = inputConfirmPass.closest('.registration__item');
-      const infoError = item.querySelector('.info-error');
-      item.classList.add('error');
-      infoError.textContent = 'Пароли не совоподают !';
+    console.log(error);
+    if (error == 0) {
+      console.log('отправка');
+    }
+  });
+  function validPhone(inputs) {
+    if (!pattern[inputs.name].test(inputs.value.trim())) {
+      addError(inputs, 'Проверте правильность ввода !');
     };
   };
+  function validName(inputs) {
+    if (!pattern[inputs.dataset.valid].test(inputs.value.trim())) {
+      addError(inputs, 'Проверте правильность ввода !');
+      console.log(inputs.dataset.valid);
+    };
+  };
+  function validPassword() {
+    if (inputPass.value !== inputConfirmPass.value) {
+      const inputPassword = document.querySelectorAll('[type="password"]')
+      inputPassword.forEach(inputsPas => addError(inputsPas, 'Пароли не совоподают !'));
+    };
+  };
+  function validEmail(inputs) {
+    if (!pattern[inputs.name].test(inputs.value.trim())) {
+      addError(inputs, 'Email неверный !');
+    };
+  };
+  function validСard(inputs) {
+    if (!pattern['card'].test(inputs.value.trim())) {
+      addError(inputs, 'Проверте правильность ввода !');
+    };
+  };
+  function addError(inputs, strError) {
+    const item = inputs.closest('.registration__item');
+    const infoError = item.querySelector('.info-error');
+    item.classList.add('error');
+    infoError.textContent = `${strError}`;
+    error++;
+  }
+  function removeError(inputs) {
+    const item = inputs.closest('.registration__item');
+    item.classList.remove('error');
+  }
 })
