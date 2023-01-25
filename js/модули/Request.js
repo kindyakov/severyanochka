b00ec931a774c83015d7cc2f250cd8c0b2dcad7d
kindyakov <61984222+kindyakov@users.noreply.github.com>
2023-01-25 20:07:21 +0300

    Update and rename js/modules/Request.js to js/модули/Request.js

--- js/модули/Request.js
+++ js/модули/Request.js
@@ -1,5 +1,6 @@
 const titleCatalog = ['milk-cheese-egg', 'frozen-foods', 'breed', 'baby-food', 'confectionery-products', 'drinks', 'fruits-vegetables', 'grocery', 'healthy-eating', 'meat-poultry-sausage', 'non-food-products', 'tea-coffee', 'pet-supplies'];
 const Request = async (urlOrigin) => {
+  console.log(urlOrigin);
   const url = urlOrigin + '/JSON/products.json';
   const respons = await fetch(url);
   const catalog = await respons.json();
