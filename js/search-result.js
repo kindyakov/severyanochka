b7d0d4599fd23918b6e7c44a75d3fb1b2fcdc4b8
kindyakov <kindyakov_2016@mail.ru>
2023-02-05 00:48:21 +0300

    new file:   img/sad-face.png
    	modified:   js/search-result.js
    	modified:   scss/adaptiv.scss

--- js/search-result.js
+++ js/search-result.js
@@ -1,11 +1,9 @@
 import { urlOrigin } from './modules/Links.js';
-import CardHtml from './modules/CardHtml.js';
+import { RenderCardHtml } from './modules/CardHtml.js';
 import { AddDisableCardBtn, AddDisableCardLike } from './modules/AddDisableClass.js';
 import Rating from './modules/Rating.js';
 import GetAllCards from './modules/GetAllCards.js';
 import CardsFromLS from "./modules/CardsFromLS.js";
-
-
 
 window.addEventListener('DOMContentLoaded', function () {
   const content = document.querySelector('.search-result__content');
@@ -13,11 +11,10 @@ window.addEventListener('DOMContentLoaded', function () {
   const searchResult_error = document.querySelector('.search-result__error');
   const [cardsBasket, cardsFavourites] = CardsFromLS();
 
-  let search_result = []
+  let search_result = {};
   if (JSON.parse(localStorage.getItem('search_result')) !== null) {
     search_result = JSON.parse(localStorage.getItem('search_result'));
   }
-
   searchResult_value.textContent = search_result.value;
   if (search_result.availability == false) {
     searchResult_error.style.display = 'inline';
@@ -25,28 +22,12 @@ window.addEventListener('DOMContentLoaded', function () {
   }
   fetch('JSON/products.json')
     .then(data => data.json())
-    .then(data => GetAllCards(data, search_result.productsID))
-    .then(cards => cards.forEach(card => renderCardHtml(card)))
+    .then(data => GetAllCards({ product: data, productID: search_result.productsID, byId: true }))
+    .then(cards => cards.forEach(card => RenderCardHtml(content, card, urlOrigin)))
     .catch(err => console.error(err))
-
-  function renderCardHtml(card) {
-    content.insertAdjacentHTML('beforeend', CardHtml(card.id, card.img, card.price, card.name, card.rating, card.link, card.catalog, urlOrigin));
-
-    const cardID = document.querySelector(`[id = "${card.id}"]`);
-    const cardDiscount = cardID.querySelector('.card-discount');
-    if (card.discount !== '') cardDiscount.textContent = '-' + card.discount + '%';
-    else cardDiscount.style.display = 'none';
-    if (card.price_card !== '') {
-      const cardWrapperPrice = cardID.querySelector('.card-wrapper-price');
-      cardID.querySelector('.card-price__i').textContent = 'Обычная';
-      cardWrapperPrice.insertAdjacentHTML('beforeend', `
-        <p class="card-price-text">
-          <span class="card-price__card card-price">${card.price_card}</span>
-          <i>С картой</i>
-        </p>`);
-    }
-    Rating();
-    AddDisableCardBtn(cardsBasket);
-    AddDisableCardLike(cardsFavourites);
-  }
+    .finally(() => {
+      Rating();
+      AddDisableCardBtn(cardsBasket);
+      AddDisableCardLike(cardsFavourites);
+    })
 })
